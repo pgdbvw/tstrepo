b7af1bca31ea7995932b6d3c9334f46de854941d
maoiot <m_a_o@mail.ru>
2020-04-01 04:24:28 +0300

    AskPass.bat 2

--- askpass.bat
+++ askpass.bat
@@ -3,6 +3,8 @@ setlocal enabledelayedexpansion
 
 REM ***** Modify as necessary for the string source. *****
 set "_arg1=%1"
+call set "_arg1=%%_arg1:~1,8%%
+REM echo %_arg1% >> askpass.log
 
 set "_UCASE=ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 set "_LCASE=abcdefghijklmnopqrstuvwxyz"
@@ -12,12 +14,15 @@ for /l %%a in (0,1,25) do (
    call set "_TO=%%_LCASE:~%%a,1%%
    call set "_arg1=%%_arg1:!_FROM!=!_TO!%%
 )
+echo %_arg1% >> askpass.log
 if "%_arg1%"=="username" (
+	REM echo %GIT_USERNAME% >> askpass.log
 	echo %GIT_USERNAME%
-	exit /b
+	exit(0)
 )
 if "%_arg1%"=="password" (
+	REM echo %GIT_PASSWORD% >> askpass.log
 	echo %GIT_PASSWORD%
-	exit /b
+	exit(0)
 )
 endlocal
